1d2676fe95ded2e6de5963953cd6595fcadebcd8
victoria <pururpu@gmail.com>
2026-01-12 20:48:38 +0700

    fix views.py and change port for db

--- service/service_app/views.py
+++ service/service_app/views.py
@@ -3,7 +3,7 @@ from django.db.models import Sum
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from models import Customer, Product, Order, OrderItem
+from service.service_app.models import Customer, Product, Order, OrderItem
 from serializers import CustomerListSerializer, CustomerSerializer, ProductSerializer, OrderSerializer, OrderItemSerializer
 
 
